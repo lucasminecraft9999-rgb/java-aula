3381d4ab4923386a5d3cc85e3f63ecb4b27f3137
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2025-09-04 17:09:12 -0300

    ExercicioQuaseConcluido

---
+++ Exercicio01/src/Exercicio01/Ex1.java
@@ -0,0 +1,13 @@
+package Exercicio01;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Ex1 {
+    public static void main(String[] args){
+       /*      Ex1    */
+        String Nome = "Lucas";
+        int idade = 22;
+
+        System.out.println("meu nome é: " + Nome + "Minha idade é: " + idade);
+        }
+    }
---
+++ Exercicio01/src/Exercicio01/Ex4.java
@@ -0,0 +1,14 @@
+package Exercicio01;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Seu nome é: " + nome);
+
+    }
+}
