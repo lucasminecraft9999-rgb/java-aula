267d719ffc11772e8881789c0c4ff4be2d1d4491
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2025-09-06 13:43:14 -0300

    Exercicio04Feito

--- Exercicio01/src/Exercicio01/Ex4.java
+++ Exercicio01/src/Exercicio01/Ex4.java
@@ -4,11 +4,8 @@ import java.util.Scanner;
 
 public class Ex4 {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+    double celsius = 35;
 
-        System.out.println("Digite seu nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Seu nome é: " + nome);
 
     }
 }
