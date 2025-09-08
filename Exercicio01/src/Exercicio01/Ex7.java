8d9f23a5f031b8650d55bf26703e1a2f96563779
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2025-09-08 19:37:29 -0300

    Ex7_Corrigido

--- Exercicio01/src/Exercicio01/Ex7.java
+++ Exercicio01/src/Exercicio01/Ex7.java
@@ -6,7 +6,7 @@ public class Ex7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Digite sua primeira nota: ");
+        System.out.println("Digite sua primeira nota: ");
         double nota1 = scanner.nextDouble();
 
         System.out.println("Digite sua segunda nota: ");
@@ -18,6 +18,26 @@ public class Ex7 {
         double total = (nota1 + nota2 + nota3) / 3 ;
 
         System.out.println("media sua nota é: " + total);
+
+        boolean ultrapassado = total > 10;
+
+        if (total > 7 && total <= 10){
+            System.out.println("Aprovado");
+        }
+        else if (total > 5 && total <= 7) {
+            System.out.println("Recuperaçao");
+        }
+
+        else {
+            System.out.println("Reprovado");
+        }
+
+        if(ultrapassado){
+            System.out.println("Voce ultrapassou o limite de notas");
+        }
+
+
+
 
     }
 }
