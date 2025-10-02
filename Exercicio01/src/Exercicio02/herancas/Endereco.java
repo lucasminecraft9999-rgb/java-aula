b911863e664baaf7dce04ffcd0ffdb3c4580e24b
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2025-10-02 09:59:12 -0300

    Exercicio02_alterado_feito

---
+++ Exercicio01/src/Exercicio02/herancas/Endereco.java
@@ -0,0 +1,6 @@
+package Exercicio02.herancas;
+
+public class Endereco {
+    public String endereco;
+    public String rua;
+}
---
+++ Exercicio01/src/Exercicio02/herancas/Professor.java
@@ -0,0 +1,8 @@
+package Exercicio02.herancas;
+
+public class Professor {
+   public String disciplina;
+
+
+
+}
