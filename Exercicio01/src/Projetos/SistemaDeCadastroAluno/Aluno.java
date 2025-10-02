0b3451a2cf174357fd9b335118bff6763e3ac340
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2025-10-02 10:00:38 -0300

    projetos_feito

---
+++ Exercicio01/src/Projetos/SistemaDeCadastroAluno/Aluno.java
@@ -0,0 +1,34 @@
+package Projetos.SistemaDeCadastroAluno;
+
+public class Aluno {
+    public String nome;
+    public String matricula;
+    public String curso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+
+
+}
